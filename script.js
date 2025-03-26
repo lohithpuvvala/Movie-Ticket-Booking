5600f47486c137ea38b956b5fc0203efd029684d
lohithpuvvala <lohithpuvvala@gmail.com>
2025-03-27 01:53:49 +0530

    Saved Data to Local Storage

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ function updateSelectedCount()
 {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
+    const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats',JSON.stringify(seatsIndex));
+
     const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount*ticketPrice;
